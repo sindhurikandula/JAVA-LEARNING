25a1e61e525af28311cb13b538f4efd1b88c5aba
sindhurikandula <104630183+sindhurikandula@users.noreply.github.com>
2022-05-06 11:29:05 +0530

    Add files via upload

--- InsertTest.java
+++ InsertTest.java
@@ -1,9 +1,9 @@
 
 import java.sql.*;
-import java.util.Calendar;
+
 import java.util.Scanner;
 
-import com.sbi.exceptions.ApplicantNotFoundException;
+import com.sbi.exceptions.ApplicantFoundException;
 
 public class InsertTest {
 
@@ -15,55 +15,48 @@ public class InsertTest {
 
 			Connection conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/xdb", "SA", "");
 			System.out.println("Connected to DB...");
-			
+
 			PreparedStatement ps = conn.prepareStatement("INSERT INTO BANK_APPLICANT VALUES (?,?,?,?,?,?) ");
-			
-			Scanner scan=new Scanner(System.in);
+
+			Scanner scan = new Scanner(System.in);
 			System.out.println("Enter applicant id to be inserted..");
-			int appid=scan.nextInt();
-			ps.setInt(1,appid);
-			
+			int appid = scan.nextInt();
+			ps.setInt(1, appid);
+
 			System.out.println("Enter applicant name to be inserted..");
-			ps.setString(2, scan.nextLine());
-			
-			System.out.println("-----------------");
+			ps.setString(2, scan.next());
+
 			System.out.println("\nEnter mail id to be inserted..");
-			ps.setString(3, scan.nextLine());
-			
+			ps.setString(3, scan.next());
+
 			System.out.println("Enter mobile number to be inserted..");
-			ps.setString(4, scan.nextLine());
-			
+			ps.setString(4, scan.next());
+
 			System.out.println("Enter address of applicant to be inserted..");
-			ps.setString(6, scan.nextLine());
-			
-	      
-			String s="1991-11-12";
-			Date x=Date.valueOf(s);
-					
-			Calendar cal = Calendar.getInstance();
-			java.util.Date date = cal.getTime();
-			java.sql.Date sqlDate= new java.sql.Date(date.getTime());
-			ps.setDate(5,sqlDate);
-			
-			
+			ps.setString(6, scan.next());
+
+			System.out.println("Enter DOB of applicant in format YYYY-MM-DD:");
+
+			String DOB = scan.next();
+			Date date = Date.valueOf(DOB);
+			ps.setDate(5, date);
+
 			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery("SELECT * FROM BANK_APPLICANT WHERE APPLICANT_ID="+appid);
-			
-			if(rs.next())
-			{
-				throw new ApplicantNotFoundException("Applicant already exist.....cannot be inserted");
+			ResultSet rs = st.executeQuery("SELECT * FROM BANK_APPLICANT WHERE APPLICANT_ID=" + appid);
+
+			if (rs.next()) {
+				throw new ApplicantFoundException("Applicant already exist.....cannot be inserted");
+			} else {
+
+				System.out.println("prepared statement is created..." + ps);
+				int row = ps.executeUpdate();
+				System.out.println("row inserted..." + row);
 			}
-			else
-			{	
-		    System.out.println("prepared statement is created..."+ps);
-			int row=ps.executeUpdate();
-			System.out.println("row inserted..."+row);
-			}
-			
-            rs.close();
-            st.close();
+
+			rs.close();
+			st.close();
 			ps.close();
-			
+
 			conn.close();
 			System.out.println("DisConnected from the db....");
 
@@ -72,10 +65,9 @@ public class InsertTest {
 		catch (SQLException e) {
 
 			e.printStackTrace();
-		}
-		catch(ApplicantNotFoundException e) {
-			   e.printStackTrace();
+		} catch (ApplicantFoundException e) {
+			e.printStackTrace();
 
-        }
-}
+		}
+	}
 }
