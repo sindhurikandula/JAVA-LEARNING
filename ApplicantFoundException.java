26ad4ca8f8bf6eb26349f84f8c5faacaad88645f
sindhurikandula <104630183+sindhurikandula@users.noreply.github.com>
2022-05-06 11:31:55 +0530

    Add files via upload

---
+++ ApplicantFoundException.java
@@ -0,0 +1,10 @@
+package com.sbi.exceptions;
+
+public class ApplicantFoundException extends Exception {
+
+	public ApplicantFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+  
+}
---
+++ ApplicantNotFoundException.java
@@ -0,0 +1,11 @@
+package com.sbi.exceptions;
+
+public class ApplicantNotFoundException extends Exception
+{
+
+	public ApplicantNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
